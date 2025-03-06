5646cce77ea6e8eb90170b9205006d6cdea4c7e8
souzafcharles <souzach@outlook.com>
2025-03-05 21:15:48 -0300

    chore: project requirements and configurations

--- src/main/java/com/souza/charles/BookstoreAPI/BookstoreApiApplication.java
+++ src/main/java/com/souza/charles/BookstoreAPI/BookstoreApiApplication.java
@@ -1,5 +1,6 @@
 package com.souza.charles.BookstoreAPI;
 
+import com.souza.charles.BookstoreAPI.environments.LoadEnvironment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookstoreApiApplication {
 
 	public static void main(String[] args) {
+		LoadEnvironment.loadEnv();
 		SpringApplication.run(BookstoreApiApplication.class, args);
 	}
 
